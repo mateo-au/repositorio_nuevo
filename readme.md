210379aabb504a4ee22ac258a458522fca1d5e44
Mateo Aubert <mateo.aubert.1@gmail.com>
2025-05-16 10:51:25 +0200

    Initial commit: added basic files

---
+++ readme.md
@@ -0,0 +1,1 @@
+Esta es la entrega del Tema19 de Entornos
---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+
+function anadirNumero(num){ 
+    var valorPantalla= document.getElementById("pantalla").value;
+    document.getElementById("pantalla").value = valorPantalla + num;
+    /*valorPantalla+=num*/
+
+}
+
+function resultado(){
+    var pantalla = document.getElementById("pantalla");
+
+    pantalla.value = (eval(pantalla.value));
+
+}
+
+function anadirNumero(num){ 
+    var valorPantalla= document.getElementById("pantalla").value;
+    document.getElementById("pantalla").value = valorPantalla + num;
+    /*valorPantalla+=num*/
+
+}
+
+function resultado(){
+    var pantalla = document.getElementById("pantalla");
+
+    pantalla.value = (eval(pantalla.value));
+}
